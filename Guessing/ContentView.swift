bbe903fffc3cefcc88eba24d07a2daed8e606ab5
Lampros Gidarakos <lgidarakos@gmail.com>
2022-07-13 14:17:17 +0300

    Initial Commit

---
+++ Guessing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guessing
+//
+//  Created by lampros.gidarakos on 13/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guessing/GuessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingApp.swift
+//  Guessing
+//
+//  Created by lampros.gidarakos on 13/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
