3aff6b0ad7f69e879ac7161fc5f607bd351cda5e
Lampros Gidarakos <lgidarakos@gmail.com>
2022-07-14 11:43:40 +0300

    Buttons

--- Guessing/ContentView.swift
+++ Guessing/ContentView.swift
@@ -9,9 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Delete selection") {
-            print("Now deleting...")
+        VStack {
+            Button("Button 1") {}
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) {}
+                .buttonStyle(.bordered)
+            Button("Button 3") {}
+                .buttonStyle(.borderedProminent)
+            Button("Button 4", role: .destructive) {}
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            
+            Button {
+                print("Button was tapped")
+            } label: {
+                Text("Tap me")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.red)
+            }
+            
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+            }
         }
+    }
+    
+    func executeDelete() {
+        print("Now deleting")
     }
 }
 
