0bd053945a0f78941f1e4d7f5f81dc1a18864e1e
Lampros Gidarakos <lgidarakos@gmail.com>
2022-07-15 10:37:49 +0300

    Alerts

--- Guessing/ContentView.swift
+++ Guessing/ContentView.swift
@@ -8,32 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            Button("Button 1") {}
-                .buttonStyle(.bordered)
-            Button("Button 2", role: .destructive) {}
-                .buttonStyle(.bordered)
-            Button("Button 3") {}
-                .buttonStyle(.borderedProminent)
-            Button("Button 4", role: .destructive) {}
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-            
-            Button {
-                print("Button was tapped")
-            } label: {
-                Text("Tap me")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(.red)
-            }
-            
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil")
-            }
+        Button("Show alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
         }
     }
     
